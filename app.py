603ad12231fa8a8557b03cc37287516abb44c336
carlos2007calin-commits <carlos2007calin@gmail.com>
2026-05-07 08:55:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,61 @@
 from flask import Flask, render_template, request
-import sqlite3
+import psycopg2
+import os
 
 app = Flask(__name__)
 
-# Crear base de datos
-def init_db():
-    conexion = sqlite3.connect("cafeteria.db")
-    cursor = conexion.cursor()
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS pedidos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT,
-        bebida TEXT
-    )
-    """)
-    conexion.commit()
-    conexion.close()
+# ================= CONEXION BASE DE DATOS =================
 
-init_db()
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+conexion = psycopg2.connect(DATABASE_URL)
+cursor = conexion.cursor()
+
+# Crear tabla si no existe
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS pedidos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT,
+    bebida TEXT
+)
+""")
+
+conexion.commit()
+
+# ================= PAGINA PRINCIPAL =================
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+
     mensaje = ""
 
     if request.method == "POST":
+
         nombre = request.form["nombre"]
         bebida = request.form["bebida"]
 
-        if nombre == "":
-            mensaje = "Ingrese nombre"
-        else:
-            conexion = sqlite3.connect("cafeteria.db")
-            cursor = conexion.cursor()
+        if nombre != "":
+
             cursor.execute(
-                "INSERT INTO pedidos (nombre, bebida) VALUES (?, ?)",
+                "INSERT INTO pedidos (nombre, bebida) VALUES (%s, %s)",
                 (nombre, bebida)
             )
+
             conexion.commit()
-            conexion.close()
 
             mensaje = f"Pedido para {nombre}: {bebida}"
 
-    return render_template("index.html", mensaje=mensaje)
+    # MOSTRAR PEDIDOS
+    cursor.execute("SELECT * FROM pedidos ORDER BY id DESC")
+    pedidos = cursor.fetchall()
+
+    return render_template(
+        "index.html",
+        mensaje=mensaje,
+        pedidos=pedidos
+    )
+
+# ================= INICIAR APP =================
 
 if __name__ == "__main__":
     app.run(debug=True)
